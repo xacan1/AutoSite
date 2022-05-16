25375839cfc376679a6bde047cdc1d3fc7d04530
Hasan <xacan-mail@mail.ru>
2022-05-17 01:09:09 +0500

    add robots

--- autobase/urls.py
+++ autobase/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from main.views import PageNotFound
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('hasan_admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('autonorms.urls')),
 ]
