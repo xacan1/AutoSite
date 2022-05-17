259db2703ccd0778d112ccc182ff871f9708af0a
Hasan <xacan-mail@mail.ru>
2022-05-17 20:45:41 +0500

    edit 404 code response

--- main/views.py
+++ main/views.py
@@ -13,6 +13,11 @@ class PageNotFound(FormView):
     form_class = SimpleForm
     template_name = 'main/page404.html'
 
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        response.status_code = 404
+        return response
+
 
 class LoginUser(DataMixin, auth_views.LoginView):
     form_class = LoginUserForm
